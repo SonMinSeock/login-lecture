52b9163815f4f702cf990aa9ac8fa9795a34e996
SonMinSeock <zonins3@gmail.com>
2022-02-10 17:30:48 +0900

    돔을 이용하여 HTML객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,16 @@
 "use strict";
-console.log("Hello");
-console.log("Bye~~");
+
+const id = document.querySelector("#id"); // querySelector() : 질의 선택자 (선택자를 통해 제어 하고자 한다.)
+const password = document.querySelector("#password"); // querySelector() : 질의 선택자 (선택자를 통해 제어 하고자 한다.)
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    // 로그인버튼 클릭할때 id, password 값을 기지고 와야한다.
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+    console.log(req);
+}
